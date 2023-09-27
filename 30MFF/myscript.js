bcc512b89b946b68fe1a4ddeee337aa685a9e2b7
naijahsimon <144206946+naijahsimon@users.noreply.github.com>
2023-09-27 12:45:50 +0400

    please god

---
+++ 30MFF/myscript.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.body.style.backgroundColor = "rgba((90,90,90,0.5)";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "black";
+  }
