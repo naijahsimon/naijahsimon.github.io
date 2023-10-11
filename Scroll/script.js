71b97a4033b427ecea5b01595c85c9f603af6ee4
naijahsimon <144206946+naijahsimon@users.noreply.github.com>
2023-10-11 14:19:36 +0400

    Scroll site

---
+++ Scroll/script.js
@@ -0,0 +1,38 @@
+
+
+$(document).ready(function(){
+    $("blocks").click(function(){
+      $("#div1").fadeTo(1000, 0.15);
+      $("#div2").fadeTo(1000, 0.4);
+      $("#div3").fadeTo(1000, 0.7);
+      $("#div4").fadeTo(1000, 0.7);
+      $("#div5").fadeTo(1000, 0.7);
+      $("#div6").fadeTo(1000, 0.7);
+    });
+  });
+
+  AOS.init()
+
+
+//let beginStory = document.getElementById("begin-reading")
+//beginStory.addEventListener('click', () => {}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollButton = document.getElementById("scrollButton");
+  const scrollTarget = document.getElementById("scrollTarget");
+  scrollButton.style.display = "none";
+  scrollButton.addEventListener("click", function () {
+      scrollTarget.scrollIntoView({
+          behavior: "smooth",
+      });
+
+      scrollButton.style.display = "none";
+  });
+  window.addEventListener("scroll", function () {
+      if (window.scrollY > 100) {
+          scrollButton.style.display = "block";
+      } else {
+          scrollButton.style.display = "none";
+      }
+  });
+});
